ae45d6d439fff621364ee278900e11f9c67d1f30
Dominic Meyer <dominic.meyer@me.com>
2021-01-28 18:05:21 +0100

    solving error when logging out

--- school-courses-app/src/components/UserSignOut.js
+++ school-courses-app/src/components/UserSignOut.js
@@ -1,10 +1,10 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import { Redirect } from 'react-router-dom';
 import Context from "../Context";
 
 export default function UserSignOut () {
   const context = useContext(Context.Context)
-  context.actions.signOut();
+  useEffect( () => context.actions.signOut());
 
   return (
     <Redirect to="/" />
